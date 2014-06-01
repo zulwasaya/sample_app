cdcdec49e1aad39f078b723f068351664373b5af
Zul Wasaya <zul.wasaya@gmail.com>
2014-06-01 22:02:53 +0100

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
   # root 'welcome#index'
 
   # Example of regular route:
